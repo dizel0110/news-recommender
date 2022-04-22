f7032468092f78a1e5daccd991cb3028a351c5d6
Pavel Polovnikov <elessar91@mail.ru>
2022-04-22 16:52:46 +0300

    add test for keys in json storage

--- tests/parser/test_habr.py
+++ tests/parser/test_habr.py
@@ -2,10 +2,20 @@ import json
 from yanr.parser.habr import HabrParser
 
 
-def test_parser(request, monkeypatch):
+def test_habr_parser_status200(request, monkeypatch):
     monkeypatch.chdir(request.fspath.dirname)
     p = HabrParser()
     p()
     with open('habr.json') as f:
         d = json.load(f)
     assert d['status'] == 200
+
+
+def test_habr_parser_correct_keys_in_json(request, monkeypatch):
+    monkeypatch.chdir(request.fspath.dirname)
+    p = HabrParser()
+    p()
+    with open('habr.json') as f:
+        d = json.load(f)
+    assert 'status' in d.keys()
+    assert 'news' in d.keys()
