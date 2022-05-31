53267e04e46ff1c802d9205460e6d8f4c11b3185
romanzes637 <romanbutov637@gmail.com>
2022-05-31 19:33:12 +0300

    workflow dvc w2v test user input added preprocess in-memory preprocessing added lemmatize renamed to normalize

--- yanr/workflow/dvc/word2vec/test.py
+++ yanr/workflow/dvc/word2vec/test.py
@@ -3,11 +3,14 @@ import json
 from datetime import datetime
 import sys
 import uuid
+from pprint import pprint
 
 from gensim.models import Word2Vec
 import pandas as pd
 import numpy as np
 import yaml
+
+from preprocess import Preprocessor
 
 
 class Tester:
@@ -15,8 +18,7 @@ class Tester:
             self,
             kind='yanr',
             raw_path='data/raw/yanr_10k.csv',
-            processed_path='data/processed/yanr_10k.txt',
-            map_path='data/processed/yanr_10k_map.txt',
+            preprocessor_params='params_preprocess.yaml',
             model_path='models/word2vec.bin.gz',
             output_path='reports/models/test.csv',
             metrics_path='reports/models/test_metrics.json',
@@ -24,8 +26,7 @@ class Tester:
             top_n=20):
         self.kind = kind
         self.raw_path = raw_path
-        self.processed_path = processed_path
-        self.map_path = map_path
+        self.preprocessor_params = preprocessor_params
         self.model_path = model_path
         self.output_path = output_path
         self.metrics_path = metrics_path
@@ -40,22 +41,19 @@ class Tester:
         model = Word2Vec.load(self.model_path)
         if self.kind == 'yanr':
             df = pd.read_csv(self.raw_path)
-            cols = ['title', 'summary', 'link', 'published']
+            data_cols = ['title', 'summary']
+            docs = [[x[y] for y in data_cols] for _, x in df.iterrows()]
+            meta_cols = ['link', 'published']
+            meta = [{y: x[y] for y in meta_cols} for _, x in df.iterrows()]
+        elif self.kind == 'list':
+            docs = self.raw_path
+            meta = [{} for _ in docs]
         else:
             raise NotImplementedError(self.kind)
-        print(f'Number of texts: {len(df)}')
-        with open(self.processed_path, encoding='utf-8') as f:
-            processed_data = f.readlines()
-        print(f'Number of sentences: {len(processed_data)}')
-        data_map = {}
-        with open(self.map_path) as f:
-            for line in f:
-                tokens = [int(x) for x in line.split()]
-                data_map.setdefault(tokens[0], []).append(tokens[1])
-        print(f'Number of maps: {len(data_map)}')
+        print(f'Number of texts: {len(docs)}')
         output_path = Path(self.output_path)
-        report_cols = ['session', 'user', 'sample', 'datetime',
-                       'a', 'rank', 'b', 'distance', 'duration', 'score']
+        report_cols = ['session', 'user', 'sample', 'datetime', 'text',
+                       'a', 'b', 'rank', 'distance', 'duration', 'score']
         if not output_path.exists():
             output_path.parent.mkdir(exist_ok=True, parents=True)
             results = pd.DataFrame(columns=report_cols)
@@ -74,13 +72,22 @@ class Tester:
         dim = model.vector_size
         print(f'Embeddings dim: {dim}')
         embeddings = []
-        sentences = []
-        for i, row in df.iterrows():
-            rows = data_map.get(i, [])
-            ss = [processed_data[x] for x in rows]
-            sentences.append(ss)
-            vs = [model.wv[y] if y in model.wv else np.zeros(dim)
-                  for x in ss for y in x.split()]
+        with open(self.preprocessor_params) as f:
+            params = yaml.safe_load(f)
+            params['kind'] = 'list'
+            params['raw_path'] = docs
+            params['processed_path'] = None
+            params['map_path'] = None
+            params['report_path'] = None
+            params['words_path'] = None
+            params['words_lengths_path'] = None
+            params['sentences_lengths_path'] = None
+        pp = Preprocessor(**params)
+        ss = pp()  # convert documents to sentences
+        for i, d in enumerate(ss):
+            # For text in document for sentence in text for word in sentence
+            vs = [model.wv[z] if z in model.wv else np.zeros(dim)
+                  for x in d for y in x for z in y]
             if len(vs) == 0:
                 embeddings.append(np.zeros(dim))
             else:
@@ -93,20 +100,59 @@ class Tester:
                 start_time = datetime.utcnow()
                 cnt += 1
                 print(f'\nSample {cnt}')
-                a = rng.integers(0, len(df))
-                rank = rng.integers(1, self.top_n + 1)
-                print('Are texts related?'
-                      '\nPrint score from 0 (unrelated) to 1 (related) or -1 (other)'
-                      '\nPrint CTRL+D to stop the session')
-                dists = np.linalg.norm(embeddings - embeddings[a], axis=1)
-                indexes = np.argsort(dists)
-                b = indexes[rank]
-                print('\nText A')
-                for c in cols:
-                    print(f'{c}: {df[c].iloc[a]}')
-                print('\nText B')
-                for c in cols:
-                    print(f'{c}: {df[c].iloc[b]}')
+                kind = rng.integers(0, 2)
+                if kind == 0:  # Text to text
+                    print('Are texts related?'
+                          '\nPrint score from 0 (unrelated) to 1 (related) '
+                          'or -1 (other)'
+                          '\nPrint CTRL+D to stop the session')
+                    text = None
+                    a = rng.integers(0, len(docs))
+                    rank = rng.integers(1, self.top_n + 1)
+                    dists = np.linalg.norm(embeddings - embeddings[a], axis=1)
+                    indexes = np.argsort(dists)
+                    b = indexes[rank]
+                    print('\nText A')
+                    pprint(docs[a])
+                    pprint(meta[a])
+                    print('\nText B')
+                    pprint(docs[b])
+                    pprint(meta[b])
+                else:  # User to text
+                    while True:
+                        try:
+                            text = input('Input some text to find related text: ')
+                            text_ds = [[text]]
+                            pp.raw_path = text_ds
+                            text_ss = pp()
+                            vs = [model.wv[z] if z in model.wv else np.zeros(dim)
+                                  for x in text_ss[0] for y in x for z in y]
+                            if len(vs) == 0:
+                                emb = np.zeros(dim)
+                            else:
+                                emb = np.mean(vs, axis=0)
+                        except (EOFError, KeyboardInterrupt):
+                            print('Interrupted')
+                            sys.exit(0)
+                        except Exception:
+                            print('Something went wrong... Try another (longer) text!')
+                        else:
+                            break
+                    print('Is this text related to your text?'
+                          '\nPrint score from 0 (unrelated) to 1 (related) '
+                          'or -1 (other)'
+                          '\nPrint CTRL+D to stop the session')
+                    rank = rng.integers(1, self.top_n + 1)
+                    dists = np.linalg.norm(embeddings - emb, axis=1)
+                    indexes = np.argsort(dists)
+                    a = None
+                    b = indexes[rank]
+                    print('\nText A')
+                    pprint(text_ds[0])
+                    pprint({'processed_text': text_ss[0]})
+                    print('\nText B')
+                    pprint(docs[b])
+                    pprint(meta[b])
                 while True:
                     try:
                         score = input()
@@ -126,6 +172,7 @@ class Tester:
                 trial['user'] = user
                 trial['sample'] = cnt
                 trial['datetime'] = start_time
+                trial['text'] = text
                 trial['a'] = a
                 trial['b'] = b
                 trial['rank'] = rank
@@ -135,7 +182,7 @@ class Tester:
                 trial = pd.DataFrame([trial])
                 results = pd.concat([results, trial])
                 results.to_csv(output_path, index=False)
-                r = results.dropna()
+                r = results.dropna(subset=['score'])
                 metrics['mean'] = r['score'].mean()
                 metrics['average_rank'] = np.average(r['score'], weights=1 / r['rank'])
                 inv_distance = np.divide(1, r['distance']).astype(float)
